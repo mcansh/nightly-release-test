4634aaa10e78bb7a09448533157a2f1191787345
Logan McAnsh <logan@mcan.sh>
2022-04-20 15:04:30 -0400

    update error message
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/constants.mjs
+++ scripts/constants.mjs
@@ -8,7 +8,9 @@ if (!process.env.GITHUB_REF) {
   throw new Error("GITHUB_REF is required");
 }
 if (!process.env.GITHUB_REF.startsWith("refs/tags/")) {
-  throw new Error("GITHUB_REF must be a tag");
+  throw new Error(
+    "GITHUB_REF must be a tag received " + process.env.GITHUB_REF
+  );
 }
 
 export const [OWNER, REPO] = process.env.GITHUB_REPOSITORY.split("/");
