214dac936ae82da4d260af241ba5766bfc79cedf
Logan McAnsh <logan@mcan.sh>
2022-07-18 17:30:36 -0400

    ci: bring back tag check and clean up
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -13,9 +13,12 @@ if (!process.env.GITHUB_REPOSITORY) {
 if (!process.env.VERSION) {
   throw new Error("VERSION is required");
 }
+if (!process.env.VERSION.startsWith("refs/tags/")) {
+  throw new Error("VERSION must be a tag, received " + process.env.VERSION);
+}
 
 export const [OWNER, REPO] = process.env.GITHUB_REPOSITORY.split("/");
-export const VERSION = process.env.VERSION;
+export const VERSION = process.env.VERSION.replace("refs/tags/", "");
 export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 export const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
 export const DEFAULT_BRANCH = process.env.DEFAULT_BRANCH;
