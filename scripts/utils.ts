8a8d6705b528f5ab00226018ff8c827afecc7fdc
Logan McAnsh <logan@mcan.sh>
2022-07-18 17:50:10 -0400

    filter and clean up tag names
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,4 +1,4 @@
-import { GITHUB_REPOSITORY } from "./constants";
+import { GITHUB_REPOSITORY, PACKAGE_TO_WATCH } from "./constants";
 
 export function checkIfStringStartsWith(
   string: string,
@@ -20,4 +20,14 @@ export function sortByDate(a: MinimalTag, b: MinimalTag) {
 export function getGitHubUrl(type: "pull" | "issue", number: number) {
   let segment = type === "pull" ? "pull" : "issues";
   return `https://github.com/${GITHUB_REPOSITORY}/${segment}/${number}`;
+}
+
+export function cleanupTagName(tagName: string) {
+  return PACKAGE_TO_WATCH
+    ? tagName.replace(`${PACKAGE_TO_WATCH}@`, "")
+    : tagName;
+}
+
+export function cleanupRef(ref: string) {
+  return cleanupTagName(ref.replace("refs/tags/", ""));
 }
