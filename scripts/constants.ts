4fd0ad62776902338ad697e02ead883a0923d4c5
Logan McAnsh <logan@mcan.sh>
2022-07-18 16:37:26 -0400

    ci: get version from `npm info`
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -13,12 +13,9 @@ if (!process.env.GITHUB_REPOSITORY) {
 if (!process.env.VERSION) {
   throw new Error("VERSION is required");
 }
-if (!process.env.VERSION.startsWith("refs/tags/")) {
-  throw new Error("VERSION must be a tag, received " + process.env.VERSION);
-}
 
 export const [OWNER, REPO] = process.env.GITHUB_REPOSITORY.split("/");
-export const REF = process.env.VERSION.replace("refs/tags/", "");
+export const VERSION = process.env.VERSION;
 export const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 export const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
 export const DEFAULT_BRANCH = process.env.DEFAULT_BRANCH;
