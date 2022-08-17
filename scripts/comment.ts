f25f28dec3e19723a40c9de96e6c19de9b967b92
Logan McAnsh <logan@mcan.sh>
2022-08-17 13:42:46 -0400

    Revert "feat: use git database to avoid some requests (#105)"
    This reverts commit 3763d00263d8fe2770576f36a05a3b8758a6ec59.

--- scripts/comment.ts
+++ scripts/comment.ts
@@ -12,7 +12,11 @@ async function commentOnIssuesAndPrsAboutRelease() {
     return;
   }
 
-  let { merged, previousTag } = await prsMergedSinceLastTag(VERSION);
+  let { merged, previousTag } = await prsMergedSinceLastTag({
+    owner: OWNER,
+    repo: REPO,
+    githubRef: VERSION,
+  });
 
   let suffix = merged.length === 1 ? "" : "s";
   let prFilesDirs = PR_FILES_STARTS_WITH.join(", ");
@@ -28,7 +32,14 @@ async function commentOnIssuesAndPrsAboutRelease() {
   for (let pr of merged) {
     console.log(`commenting on pr ${getGitHubUrl("pull", pr.number)}`);
 
-    promises.push(commentOnPullRequest({ pr: pr.number, version: VERSION }));
+    promises.push(
+      commentOnPullRequest({
+        owner: OWNER,
+        repo: REPO,
+        pr: pr.number,
+        version: VERSION,
+      })
+    );
 
     let issuesClosed = await getIssuesClosedByPullRequests(
       pr.html_url,
@@ -43,7 +54,14 @@ async function commentOnIssuesAndPrsAboutRelease() {
       }
       issuesCommentedOn.add(issueNumber);
       console.log(`commenting on issue ${getGitHubUrl("issue", issueNumber)}`);
-      promises.push(commentOnIssue({ issue: issueNumber, version: VERSION }));
+      promises.push(
+        commentOnIssue({
+          issue: issueNumber,
+          owner: OWNER,
+          repo: REPO,
+          version: VERSION,
+        })
+      );
     }
   }
 
