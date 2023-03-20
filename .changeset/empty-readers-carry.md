f93519196f9870f808b3df598dc586c8d1680f44
Logan McAnsh <logan@mcan.sh>
2023-03-20 12:46:20 -0400

    feat: add new package (#171)
    * chore(deps): bump to latest
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * feat: add new package
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * Create .changeset/empty-readers-carry.md
    
    * chore(deps): bump to latest
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    (cherry picked from commit bd08456bfa11bd6e1fe7bb7171731a5975eb8263)
    
    ---------
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ .changeset/empty-readers-carry.md
@@ -0,0 +1,5 @@
+---
+"@mcansh/nightly-release-test": patch
+---
+
+feat: add new package
---
+++ packages/three/src/index.ts
@@ -0,0 +1,2 @@
+export { one } from "@mcansh/nightly-release-test-one";
+export { two } from "@mcansh/nightly-release-test-two";
