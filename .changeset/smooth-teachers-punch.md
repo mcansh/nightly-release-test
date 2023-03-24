1d9747ec32f8f8e9e681a96980751a9df49847ef
Logan McAnsh <logan@mcan.sh>
2023-03-24 14:44:52 -0400

    add main fn (#180)
    * add main fn
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * Create .changeset/smooth-teachers-punch.md
    
    ---------
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ .changeset/smooth-teachers-punch.md
@@ -0,0 +1,5 @@
+---
+"@mcansh/nightly-release-test": patch
+---
+
+add main fn
--- packages/_main/src/index.ts
+++ packages/_main/src/index.ts
@@ -1,3 +1,11 @@
-export { one } from "@mcansh/nightly-release-test-one";
-export { two } from "@mcansh/nightly-release-test-two";
-export { three } from "@mcansh/nightly-release-test-three";
+import { one } from "@mcansh/nightly-release-test-one";
+import { two } from "@mcansh/nightly-release-test-two";
+import { three } from "@mcansh/nightly-release-test-three";
+
+export { one, two, three };
+
+export function main() {
+  one();
+  two();
+  three();
+}
